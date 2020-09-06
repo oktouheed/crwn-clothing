2a8ff238de1ee47c7dfd3a296d6cf6cfb843788d
oktouheed <oktouheed@gmail.com>
2020-09-07 00:52:45 +0600

    Updating ouor app to redirect to home if user is signed in

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,10 +1,12 @@
+import { UserActionTypes } from './user.types';
+
 const INITIAL_STATE = {
     currentUser: null
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {
-    switch(action.typ) {
-        case 'SET_CURRENT_USER':
+    switch(action.type) {
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload
